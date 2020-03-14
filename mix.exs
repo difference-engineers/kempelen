3a3ec2622491e86b2dc38f17d3ecf4e4b8a51c1d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-03-14 01:19:16 -0700

    Credo does analysis, only load these other gems in dev/test

--- mix.exs
+++ mix.exs
@@ -57,8 +57,9 @@ defmodule Kempelen.MixProject do
       {:oban, "~> 1.0"},
       {:flippant, "~> 1.0"},
       {:google_maps, "~> 0.11"},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 
