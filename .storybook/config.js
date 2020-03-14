7c30ae28cd97088168b9334749c96978231b2d78
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-03-14 01:20:59 -0700

    Not ready for this yet

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,12 @@
 import React from "react"
-import {configure, addDecorator} from "@storybook/react"
-import {withKnobs} from "@storybook/addon-knobs"
+// import {Provider} from "react-redux"
+import {addDecorator} from "@storybook/react"
+import {configure} from '@storybook/react'
 import {checkA11y} from "@storybook/addon-a11y"
-import {withTests} from "@storybook/addon-jest"
-import results from "./jest-test-results.json"
-import {Provider} from "react-redux"
-import rematch from "@internal/rematch"
-import {configure} from '@storybook/react';
+import {withKnobs} from "@storybook/addon-knobs"
+// import {withTests} from "@storybook/addon-jest"
+// import rematch from "@internal/rematch"
+// import results from "./jest-test-results.json"
 
 // automatically import all files ending in *.stories.js
 const requireWithContext = require.context('../@internal', true, /story\.js$/);
@@ -14,10 +14,10 @@ const requireWithContext = require.context('../@internal', true, /story\.js$/);
 addDecorator(withTests({results}))
 addDecorator(withKnobs)
 addDecorator(checkA11y)
-addDecorator((story) => {
-  return <Provider store={rematch}>
-    {story()}
-  </Provider>
-})
+// addDecorator((story) => {
+//   return <Provider store={rematch}>
+//     {story()}
+//   </Provider>
+// })
 
 configure(() => requireWithContext.keys().forEach((filename) => requireWithContext(filename)), module);
