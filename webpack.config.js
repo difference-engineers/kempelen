f2140df650f7427ba9300898b50b897af3d1b70c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-03-15 19:26:12 -0700

    Reconfiguring

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ dotenvConfiguration();
 
 const BENCHMARK = process.env.BENCHMARK === "enabled";
 const NODE_ENV = process.env.NODE_ENV || "development";
-const DEVTOOL = NODE_ENV === "production" ? "source-map" : "inline-source-map";
+const PRODUCTION = NODE_ENV === "production";
+const DEVTOOL = PRODUCTION ? "source-map" : "inline-source-map";
 const PACKAGE_ASSETS = [];
 const SHARED_BUILD_CONFIGURATION = {
   mode: NODE_ENV,
@@ -40,6 +41,30 @@ const SHARED_BUILD_CONFIGURATION = {
     ],
   },
 };
+const PRE_SHARED_PLUGINS = [
+  PRODUCTION ? null : new DotenvWebpack(),
+  new EnvironmentPlugin([
+    "NODE_ENV",
+    "BENCHMARK",
+    "COUCHDB_USERNAME",
+    "COUCHDB_PASSWORD",
+    "COUCHDB_URI",
+  ]),
+  PRODUCTION ? new CleanWebpackPlugin({verbose: true}) : null,
+];
+const POST_SHARED_PLUGINS = [
+  PRODUCTION ? new CompressionWebpackPlugin({
+    filename: "[path].br[query]",
+    algorithm: "brotliCompress",
+    compressionOptions: {level: 11},
+    test: /\.(?:js|css|txt|xml|json|png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/iu,
+  }) : null,
+  PRODUCTION ? new CompressionWebpackPlugin({
+    test: /\.(?:js|css|txt|xml|json|png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/iu,
+  }) : null,
+  new WebpackCommonShake(),
+  BENCHMARK ? new BundleAnalyzerPlugin({analyzerMode: "static", openAnalyzer: false}) : null,
+];
 
 function clientFor (name, target, configuration = {}) {
   return mergeDeepRight({
@@ -49,11 +74,14 @@ function clientFor (name, target, configuration = {}) {
     ],
     target,
     output: {
-      path: path.resolve(__dirname, "tmp", name),
+      // publicPath: '//cdn.example.com'
+      publicPath: "/assets/",
+      path: path.resolve(__dirname, "tmp", name.split("-")[0], "assets"),
       filename: "[name].[hash].js",
+      chunkFilename: "[id].[chunkhash].js",
     },
     optimization: {
-      minimize: NODE_ENV === "production",
+      minimize: PRODUCTION,
       runtimeChunk: "single",
       splitChunks: {
         minSize: 0,
@@ -129,24 +157,24 @@ function clientFor (name, target, configuration = {}) {
       },
     },
     devServer: {
-      contentBase: path.join(__dirname, "tmp", name),
+      contentBase: path.join(__dirname, "tmp", name.split("-")[0]),
       compress: true,
       historyApiFallback: true,
       hot: true,
       overlay: true,
     },
     plugins: compact([
-      NODE_ENV === "production" ? null : new DotenvWebpack(),
-      NODE_ENV === "production" ? null : new HotModuleReplacementPlugin(),
-      new EnvironmentPlugin([
-        "NODE_ENV",
-        "BENCHMARK",
-        "COUCHDB_USERNAME",
-        "COUCHDB_PASSWORD",
-        "COUCHDB_URI",
-      ]),
-      NODE_ENV === "production" ? new IgnorePlugin(/^\.\/locale$/u, /moment$/u) : null,
-      NODE_ENV === "production" ? new CleanWebpackPlugin({verbose: true}) : null,
+      ...PRE_SHARED_PLUGINS,
+      new CopyWebpackPlugin([{
+        from: path.resolve(__dirname, "assets"),
+        to: path.resolve(__dirname, "tmp", name.split("-")[0], "assets"),
+      }]),
+      ...PACKAGE_ASSETS.map(([from, ...to]) => new CopyWebpackPlugin([{
+        from,
+        to: path.resolve(__dirname, "tmp", name.split("-")[0], "assets", ...to),
+      }])),
+      PRODUCTION ? null : new HotModuleReplacementPlugin(),
+      PRODUCTION ? new IgnorePlugin(/^\.\/locale$/u, /moment$/u) : null,
       new HtmlWebpackPlugin({
         minify: {
           removeComments: true,
@@ -158,36 +186,18 @@ function clientFor (name, target, configuration = {}) {
           minifyCSS: true,
         },
         hash: true,
-        template: path.join(name, "..", "templates", "index.html"),
+        template: path.join(__dirname, "templates", "index.html"),
       }),
       new HashedModuleIdsPlugin(),
-      new CopyWebpackPlugin([{
-        from: path.resolve(__dirname, "assets"),
-        to: path.resolve(__dirname, "tmp", name, "assets"),
-      }]),
-      ...PACKAGE_ASSETS.map(([from, ...to]) => new CopyWebpackPlugin([{
-        from,
-        to: path.resolve(__dirname, "tmp", name, ...to),
-      }])),
-      new WebpackCommonShake(),
-      NODE_ENV === "production" ? new WebpackSubresourceIntegrity({
+      PRODUCTION ? new WebpackSubresourceIntegrity({
         hashFuncNames: ["sha256", "sha384"],
         enabled: true,
       }) : null,
-      NODE_ENV === "production" ? new CompressionWebpackPlugin({
-        filename: "[path].br[query]",
-        algorithm: "brotliCompress",
-        compressionOptions: {level: 11},
-        test: /\.(?:js|css|txt|xml|json|png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/iu,
-      }) : null,
-      NODE_ENV === "production" ? new CompressionWebpackPlugin({
-        test: /\.(?:js|css|txt|xml|json|png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/iu,
-      }) : null,
       new WebpackAssetsManifest({
         output: "asset-integrity-manifest.json",
-        integrity: NODE_ENV === "production",
+        integrity: PRODUCTION,
       }),
-      BENCHMARK ? new BundleAnalyzerPlugin({analyzerMode: "static", openAnalyzer: false}) : null,
+      ...POST_SHARED_PLUGINS,
     ]),
   })(
     configuration
@@ -205,24 +215,23 @@ function serverFor (name, target, configuration = {}) {
       __filename: false,
     },
     output: {
-      path: path.resolve(__dirname, "tmp", name),
+      path: path.resolve(__dirname, "tmp", name.split("-")[0]),
     },
     optimization: {
-      minimize: NODE_ENV === "production",
+      minimize: PRODUCTION,
     },
     plugins: compact([
-      NODE_ENV === "production" ? null : new DotenvWebpack(),
-      new EnvironmentPlugin([
-        "NODE_ENV",
-        "BENCHMARK",
-        "COUCHDB_USERNAME",
-        "COUCHDB_PASSWORD",
-        "COUCHDB_URI",
-      ]),
-      NODE_ENV === "production" ? new IgnorePlugin(/^\.\/locale$/u, /moment$/u) : null,
-      NODE_ENV === "production" ? new CleanWebpackPlugin({verbose: true}) : null,
-      new WebpackCommonShake(),
-      BENCHMARK ? new BundleAnalyzerPlugin({analyzerMode: "static", openAnalyzer: false}) : null,
+      ...PRE_SHARED_PLUGINS,
+      PRODUCTION ? new IgnorePlugin(/^\.\/locale$/u, /moment$/u) : null,
+      new CopyWebpackPlugin([{
+        from: path.resolve(__dirname, "assets"),
+        to: path.resolve(__dirname, "tmp", name.split("-")[0], "assets"),
+      }]),
+      ...PACKAGE_ASSETS.map(([from, ...to]) => new CopyWebpackPlugin([{
+        from,
+        to: path.resolve(__dirname, "tmp", name.split("-")[0], "assets", ...to),
+      }])),
+      ...POST_SHARED_PLUGINS,
     ]),
   })(
     configuration
@@ -235,7 +244,7 @@ module.exports = [
       port: 9000,
     },
   }),
-  // clientFor("desktop", "electron-renderer", {
+  // clientFor("desktop-client", "electron-renderer", {
   //   devServer: {
   //     port: 9001,
   //   },
